c6ae7968eb35d0c29b253e14b2128366fcb784da
Everton Silva <thonns@gmail.com>
2018-08-26 15:41:54 +0000

    refactor(data): add anotation @Repository to book repository interface

--- src/main/java/br/com/everton/les2018/repository/BookRepository.java
+++ src/main/java/br/com/everton/les2018/repository/BookRepository.java
@@ -1,9 +1,11 @@
 package br.com.everton.les2018.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import br.com.everton.les2018.model.Book;
 
+@Repository
 public interface BookRepository extends JpaRepository<Book, Long> {
 
 }
