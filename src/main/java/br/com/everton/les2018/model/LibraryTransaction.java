6287e6c8fe760042a46bc85cea7354e9721dd276
EvertonSilva <thonns@gmail.com>
2018-09-22 16:54:46 -0300

    refactor: define 1 -> N relation between LibraryTransaction and Publication

--- src/main/java/br/com/everton/les2018/model/LibraryTransaction.java
+++ src/main/java/br/com/everton/les2018/model/LibraryTransaction.java
@@ -1,5 +1,8 @@
 package br.com.everton.les2018.model;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import javax.persistence.CascadeType;
 import javax.persistence.Embedded;
 import javax.persistence.Entity;
@@ -7,7 +10,7 @@ import javax.persistence.FetchType;
 import javax.persistence.Inheritance;
 import javax.persistence.InheritanceType;
 import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 
 @Entity
 @Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
@@ -15,20 +18,24 @@ public class LibraryTransaction extends DomainEntity {
 
 	private static final long serialVersionUID = 1L;
 	
-	@ManyToOne(fetch = FetchType.LAZY,
+	@OneToMany(fetch = FetchType.LAZY,
 				cascade = CascadeType.ALL)
 	@JoinColumn(name = "publication_id")
-	private Publication publication;
+	private Set<Publication> publications = new HashSet<>();
 	
 	@Embedded
 	private DateRange range;
 
-	public Publication getPublication() {
-		return publication;
+	public Set<Publication> getPublication() {
+		return publications;
 	}
 
-	public void setPublication(Publication publication) {
-		this.publication = publication;
+	public void setPublication(Set<Publication> publications) {
+		this.publications = publications;
+	}
+	
+	public void addPublication(Publication p) {
+		this.publications.add(p);
 	}
 
 	public DateRange getRange() {
--- src/main/java/br/com/everton/les2018/model/Reserve.java
+++ src/main/java/br/com/everton/les2018/model/Reserve.java
@@ -1,8 +1,10 @@
 package br.com.everton.les2018.model;
 
 import javax.persistence.Entity;
+import javax.persistence.Table;
 
 @Entity
+@Table(name = "reservations")
 public class Reserve extends LibraryTransaction {
 
 	private static final long serialVersionUID = 1L;
