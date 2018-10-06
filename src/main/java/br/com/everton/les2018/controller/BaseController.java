3e849aae3abd3f516bf0fb79b008c02f00741176
EvertonSilva <thonns@gmail.com>
2018-10-06 17:59:17 -0300

    refactor: Autowire HttpSession object

--- src/main/java/br/com/everton/les2018/controller/BaseController.java
+++ src/main/java/br/com/everton/les2018/controller/BaseController.java
@@ -1,5 +1,7 @@
 package br.com.everton.les2018.controller;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 
 import br.com.everton.les2018.service.CrudService;
@@ -8,4 +10,7 @@ public abstract class BaseController<T> {
 	
 	@Autowired
 	protected CrudService<T> service;
+	
+	@Autowired
+	protected HttpSession session;
 }
