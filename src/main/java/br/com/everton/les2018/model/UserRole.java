e86192f49750e2e95a109e41bee579871711a7a2
EvertonSilva <thonns@gmail.com>
2018-10-05 23:42:36 -0300

    refactor: change JPA @Inheritance strategy

--- src/main/java/br/com/everton/les2018/model/UserRole.java
+++ src/main/java/br/com/everton/les2018/model/UserRole.java
@@ -1,11 +1,15 @@
 package br.com.everton.les2018.model;
 
+import javax.persistence.DiscriminatorColumn;
 import javax.persistence.Entity;
 import javax.persistence.Inheritance;
 import javax.persistence.InheritanceType;
+import javax.persistence.Table;
 
 @Entity
-@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
+@Table(name = "user_roles")
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(name = "role_type")
 public abstract class UserRole extends DomainEntity {
 	
 	private static final long serialVersionUID = 1L;
