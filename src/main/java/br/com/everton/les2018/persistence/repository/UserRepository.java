6dc203b4b6b6493ad7c326a20b968570dcf9cd00
EvertonSilva <thonns@gmail.com>
2018-10-07 22:11:03 -0300

    refactor: add annotation @Repository

--- src/main/java/br/com/everton/les2018/persistence/repository/UserRepository.java
+++ src/main/java/br/com/everton/les2018/persistence/repository/UserRepository.java
@@ -1,7 +1,10 @@
 package br.com.everton.les2018.persistence.repository;
 
+import org.springframework.stereotype.Repository;
+
 import br.com.everton.les2018.model.User;
 
+@Repository
 public interface UserRepository extends JpaSpecificationRepository<User, Long> {
 
 }
