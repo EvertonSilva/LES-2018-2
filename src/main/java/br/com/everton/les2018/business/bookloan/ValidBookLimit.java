849c729cf1d42987402dfcd260e8939882495e03
Everton Silva <thonns@gmail.com>
2018-11-26 11:21:29 -0200

    refactor: remove unused imports

--- src/main/java/br/com/everton/les2018/business/bookloan/ValidBookLimit.java
+++ src/main/java/br/com/everton/les2018/business/bookloan/ValidBookLimit.java
@@ -11,7 +11,6 @@ import org.springframework.stereotype.Component;
 
 import java.util.List;
 import java.util.Set;
-import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicInteger;
 
 @Component
